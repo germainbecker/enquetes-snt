77b75b011e02b5eba8e044a75a7fbd3c686aadf5
germainbecker <germain.becker@gmail.com>
2021-10-27 09:12:48 +0200

    ajout autorisation conversion MD->HTML des balises titres

--- enquetes_snt/enquetes_snt/settings.py
+++ enquetes_snt/enquetes_snt/settings.py
@@ -215,6 +215,12 @@ MARKDOWNIFY = {
             'tr',
             'th',
             'td',
+            'h1',
+            'h2',
+            'h3',
+            'h4',
+            'h5',
+            'h6'
         ],
         "WHITELIST_ATTRS": [
             'href',
